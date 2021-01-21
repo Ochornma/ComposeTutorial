8e1f3daafed79358e995f9944d761fea5083aa29
Ochornma <ochornmapromise@gmail.com>
2021-01-21 17:47:42 +0100

    designed the first page

--- app/src/main/java/com/example/promisefirst/MainActivity.kt
+++ app/src/main/java/com/example/promisefirst/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.promisefirst
 
 import android.content.res.Resources
 import android.os.Bundle
+import android.text.Layout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,13 +25,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PromiseFirstTheme {
-               // body(resources = resources)
-                body2()
+                project(resources = resources)
+                //body2()
             }
 
         }
 
 
+    }
+}
+
+// The Project
+@Composable
+fun project(resources: Resources){
+    ScrollableColumn(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .background(color = Color(0XFFF2F2F2))) {
+        Image(bitmap = imageFromResource(res = resources, resId = R.drawable.happy_meal),
+            modifier = Modifier
+                .height(300.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Fit)
+        Column(modifier = Modifier.padding(16.dp)) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "Happy Meal",
+                    style = TextStyle(fontSize = TextUnit.Companion.Sp(26)))
+
+                Text(text = "$5.99",
+                    style = TextStyle(fontSize = TextUnit.Companion.Sp(17),
+                        color = Color(0XFF85bb65)),
+                modifier = Modifier.align(Alignment.CenterVertically))
+            }
+
+            Spacer(modifier = Modifier.padding(10.dp))
+            Text(text = "800 Calories",
+                style = TextStyle(fontSize = TextUnit.Companion.Sp(17), ))
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Button(onClick = {}, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                Text(text = "Order Now")
+            }
+
+        }
     }
 }
 
@@ -43,8 +84,8 @@ fun body(resources: Resources){
         .background(color = Color(0XFFF2F2F2))) {
         Image(bitmap = imageFromResource(res = resources, resId = R.drawable.happy_meal),
                 modifier = Modifier
-                        .height(300.dp)
-                        .fillMaxWidth(),
+                    .height(300.dp)
+                    .fillMaxWidth(),
                 contentScale = ContentScale.Fit)
         Column(modifier = Modifier.padding(16.dp)) {
             Text(text = "Happy Meal",
@@ -64,9 +105,9 @@ fun body(resources: Resources){
 @Composable
 fun body2() {
     Row(modifier = Modifier
-            .fillMaxWidth()
-            .height(200.dp)
-            .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
+        .fillMaxWidth()
+        .height(200.dp)
+        .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
     horizontalArrangement = Arrangement.Center
     ) {
         Text(text = "ITEM 1",
@@ -79,8 +120,8 @@ fun body2() {
 @Composable
 fun DefaultPreview(){
     PromiseFirstTheme{
-       // body(resources = Resources.getSystem())
-        body2()
+       project(resources = Resources.getSystem())
+
     }
    // body(resources = Resources.getSystem())
 
